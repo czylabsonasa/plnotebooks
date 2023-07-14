1be369749ba43eb17d0486ad420b97a9bd2495ff
Czylabson Asa <czylabson.asa@gmail.com>
2023-07-14 11:29:51 +0200

    genalg_1 discrete

--- genalg_1/main.jl
+++ genalg_1/main.jl
@@ -1,8 +1,25 @@
 ### A Pluto.jl notebook ###
-# v0.19.26
+# v0.19.27
 
 using Markdown
 using InteractiveUtils
+
+# ╔═╡ a2c302a2-bee3-4fc0-abe7-34e3e35e98be
+begin
+	import Pkg
+	Pkg.activate(".")
+
+	
+	Pkg.add.(
+		[
+			"StatsBase",
+		]
+	)
+	Pkg.instantiate()
+
+	using
+		StatsBase
+end
 
 # ╔═╡ 4c25c370-21c8-11ee-0347-43efcf43c842
 begin
@@ -13,7 +30,7 @@ end
 # ╔═╡ d5d2e576-d068-4f13-b94b-4f954a73edb6
 begin
 	md"""#### genalg_1
-	* simple discrete ga 
+	* discrete ga - 
 	"""
 end
 
@@ -21,30 +38,30 @@ end
 begin
 
 # building blocks of the problem
-TVAR=Int8
-NVAR=5
-LB,UB=-10,10
-PM=0.05
+const TVAR=Int8                           # type of the vars
+const NVAR=5                              # num of vars
+const LB,UB=-10,10                        # lower and upper bound 4 vars
+const DOM=TVAR.(collect(LB:UB))           # the domain of vars
+const PM=0.05                             # prob of mutation
 
-# a type for individuals
-mutable struct X
+mutable struct TGENE                # type for the genes
+	arr::Vector{TVAR}           
 	val::Float64
-	arr::Vector{TVAR}
 end
 
+# min -> max of the negative
 value(x::Vector{TVAR})=sum(x.^2)
-eval(x::X)=(x.val=value(x.arr))
-	
-rand(X)=(arr=TVAR.(Base.rand(LB:UB,NVAR));X(value(arr),arr))
+value!(x::TGENE)=x.val=value(x.arr)    
 
-function mutate(x::X)
-	IDX=(1:NVAR)[Base.rand(NVAR).<PM]
-	x.arr[IDX]=Base.rand(LB:UB,length(IDX))
+function mutate(x::TGENE)
+	IDX=(1:NVAR)[rand(NVAR).<PM]
+	x.arr[IDX]=rand(DOM,length(IDX))
 end
 
-function ucross(p1::X,p2::X,c1::X,c2::X)	
-	for (i,t) in enumerate(Base.rand(NVAR))
-		c1.arr[i],c2.arr[i]=if t<0.5
+
+function cross(p1::TGENE,p2::TGENE,c1::TGENE,c2::TGENE)	
+	for i in 1:NVAR
+		c1.arr[i],c2.arr[i]=if rand()<0.5
 			p1.arr[i],p2.arr[i]
 		else
 			p2.arr[i],p1.arr[i]
@@ -52,15 +69,42 @@ function ucross(p1::X,p2::X,c1::X,c2::X)
 	end
 end
 
+# random gene
+choose(TGENE)=(arr=rand(DOM,NVAR);TGENE(arr,value(arr)))
+
+
+const POP_SIZE=10
+const MAXSTEP=100
+
+function ga()
+	POP=[choose(TGENE) for k in 1:2POP_SIZE]
+	for step in 1:MAXSTEP
+		w=Weights([exp(-POP[i].val) for i in 1:POP_SIZE])
+		idx=sample(1:POP_SIZE, w, POP_SIZE; replace=true)
+		for i in 1:2:POP_SIZE
+			p1,p2,c1,c2=POP[idx[i]],POP[idx[i+1]],POP[i+POP_SIZE],POP[i+POP_SIZE+1]
+			cross(p1,p2,c1,c2)
+			mutate(c1)
+			mutate(c2)
+			value!(c1)
+			value!(c2)
+		end
+		sort!(POP; by=x->x.val)
+		println(POP[1].val)
+	end
+end
+
 end
 
 # ╔═╡ 110d4571-b454-4431-9c57-e6a033d263cd
+# ╠═╡ disabled = true
+#=╠═╡
 # try
 begin
-POP=[rand(X) for k in 1:8]
+POP=[choose(TGENE) for k in 1:8]
 for k in 1:2:4
 	p1,p2,c1,c2=POP[k],POP[k+1],POP[k+4],POP[k+4+1]
-	ucross(p1,p2,c1,c2)
+	cross(p1,p2,c1,c2)
 	mutate(c1)
 	mutate(c2)
 	eval(c1)
@@ -69,9 +113,15 @@ end
 
 POP
 end
+  ╠═╡ =#
+
+# ╔═╡ d0be687d-e3b6-4cd0-9f14-e01935717ba0
+ga()
 
 # ╔═╡ Cell order:
-# ╠═4c25c370-21c8-11ee-0347-43efcf43c842
+# ╟─4c25c370-21c8-11ee-0347-43efcf43c842
 # ╠═d5d2e576-d068-4f13-b94b-4f954a73edb6
+# ╠═a2c302a2-bee3-4fc0-abe7-34e3e35e98be
 # ╠═a6a1ba67-c880-479d-b510-c6c7617be212
 # ╠═110d4571-b454-4431-9c57-e6a033d263cd
+# ╠═d0be687d-e3b6-4cd0-9f14-e01935717ba0
